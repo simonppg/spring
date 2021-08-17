14d63cd3ef8ee159e9ff8dd5e2d246453414a362
simonppg <simonppg@gmail.com>
2021-08-17 10:19:21 -0700

    Print out all the beans

--- SpringbootTutorial/src/main/java/com/tutorial/springboot/basics/springboot/SpringbootApplication.java
+++ SpringbootTutorial/src/main/java/com/tutorial/springboot/basics/springboot/SpringbootApplication.java
@@ -2,12 +2,16 @@ package com.tutorial.springboot.basics.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootApplication.class, args);
 
+		for(String name: applicationContext.getBeanDefinitionNames()){
+			 System.out.println(name);
+		}
+	}
 }
